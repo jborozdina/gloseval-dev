179c3fe44997d115cf43a6c1977e884d1f17c117
jborozdina <julia.borozdina@drillinginfo.com>
2016-06-22 18:28:39 +0300

    separate folder for docker files

--- chrome.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#
-# Convenience start script for Google Chrome
-#
-
-# TODO This is neccesary because of a POSIX file system error when writing to ~/.config/ - fix
-google-chrome --user-data-dir=/tmp/google-chrome/ &> /tmp/google-chrome.log &
